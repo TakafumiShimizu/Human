b660c8346ce0ec6c3c1cf7aa614dd211c1d06320
Takafumi Shimizu <tkf.smz@gmail.com>
2017-11-17 02:15:45 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takafumi/shimizu/human/Animal.java
@@ -0,0 +1,12 @@
+package jp.techacademy.takafumi.shimizu.human;
+
+/**
+ * Created by tkfsm on 2017/11/17.
+ */
+
+abstract class Animal {
+    String name;
+    int age;
+
+    abstract  public void say();
+}
---
+++ app/src/main/java/jp/techacademy/takafumi/shimizu/human/Thinkable.java
@@ -0,0 +1,9 @@
+package jp.techacademy.takafumi.shimizu.human;
+
+/**
+ * Created by tkfsm on 2017/11/17.
+ */
+
+interface Thinkable {
+    public void think();
+}
